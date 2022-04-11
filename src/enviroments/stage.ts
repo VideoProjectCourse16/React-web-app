41d576c1d9652ecc05a8e8723c6675f137ad4663
MarkPari <marco.parisi@stevejobs.academy>
2022-04-11 11:12:30 +0200

    fix(enviroments/stage): change baseUrl

--- src/enviroments/stage.ts
+++ src/enviroments/stage.ts
@@ -1,6 +1,7 @@
 import React from "react";
 
 const env={
-    baseUrl:'https://videoprojectcourse16.herokuapp.com'
+    //baseUrl:'https://videoprojectcourse16.herokuapp.com'
+    baseUrl:'http://localhost:3001'
 }
 export default  env;
